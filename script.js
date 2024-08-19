f26c11067a34f6a2a99153df0e293f01f6c3fc16
Minh Le <41931581+beaver700nh@users.noreply.github.com>
2024-08-19 05:41:36 +0000

    Commit so I don't have to download

--- script.js
+++ script.js
@@ -43,6 +43,40 @@ function list_item_adder() {
   $item.insertBefore($(this));
 }
 
+function list_title_loader($elem) {
+  let $title = $("#list-title-template").clone();
+  $title.removeAttr("id");
+  $title.appendTo($elem);
+}
+
+function list_content_loader($elem) {
+  let $ul = $("#list-content-template").clone();
+  $ul.removeAttr("id");
+  $ul.children("li.list-item").remove();
+  $ul.appendTo($elem);
+}
+
+function list_loader($elem) {
+  let $list = $("#list-template").clone();
+  $list.removeAttr("id");
+  $list.click(list_remover);
+  $list.find(".add-list").click(list_adder);
+  $list.find(".add-list-item").click(list_item_adder);
+  $list.appendTo($elem);
+}
+
+function list_item_loader($elem) {
+  let $item = $("#list-item-template").clone();
+  $item.removeAttr("id");
+  $item.appendTo($elem);
+}
+
+function list_adder_loader($elem) {
+  let $btn = $("#button-add-list-toplevel-template");
+  $btn.removeAttr("id");
+  $btn.appendTo($elem);
+}
+
 function sanitize(filename) {
   let sanitized = filename
     .replace(/[^0-9A-Z_\-\.]/gi, "_")
@@ -82,10 +116,15 @@ function saver() {
 }
 
 function encode_lists($elem) {
-  if (!$elem) $elem = $("body");
+  if (!$elem) $elem = $("#main");
 
   let $sublist = $elem.children("ul");
-  let title = $elem.children("span").first().text();
+  let $title = $elem.children("span");
+  let title = null;
+  
+  if ($title) {
+    title = $title.first().text();
+  }
 
   // This is a list item, not a list
   if ($sublist.length === 0) return title;
@@ -99,12 +138,79 @@ function encode_lists($elem) {
   return encoded;
 }
 
-function loader() {
-  ///
+function loader_delegate() {
+  $("#fload").trigger("click");
 }
 
-function decode_lists() {
-  ///
+function loader() {
+  if (this.files.length === 0) return;
+
+  let reader = new FileReader();
+
+  reader.onload = () => {
+    let saved = parse_json(reader.result);
+    if (saved) decode_lists(saved);
+  };
+  reader.onerror = () => {
+    let saved = manual_upload();
+    if (!saved) return;
+
+    saved = parse_json(saved);
+    if (!saved) return;
+
+    decode_lists(saved);
+  };
+
+  reader.readAsText(this.files[0]);
+}
+
+function manual_upload() {
+  return prompt(
+    "There was an error reading the file.\n" +
+    "Try copy-pasting the file contents here instead:"
+  );
+}
+
+function parse_json(json_str) {
+  try {
+    return JSON.parse(json_str);
+  }
+  catch (e) {
+    alert("Error in uploaded file: " + e.message);
+    return null;
+  }
+}
+
+function decode_lists(saved_json, $elem) {
+  if (!saved_json) return;
+  if (!$elem) $elem = $("#main");
+
+  console.log($elem, saved_json);
+
+  $elem.empty();
+
+  list_title_loader($elem);
+
+  let $list = $(`<ul class="list-content"></ul>`);
+  $elem.append($list);
+
+  console.log("===");
+
+  for (let sub of saved_json.items) {
+    console.log(sub, "\n---");
+
+    if (typeof sub === "string") {
+      var $item = $(`<li class="list-item"><span contenteditable="true">${sub}</span></li>`);
+    }
+    else {
+      var $item = $(`<li class="list"></li>`);
+      decode_lists(sub, $item);
+    }
+
+    $list.append($item);
+  }
+
+  console.log("===");
 }
 
 function setup() {
@@ -122,7 +228,9 @@ function setup() {
 
   /* Callback for save/load buttons */
   $("#save").click(saver);
-  $("#load").click(loader);
+  $("#load").click(loader_delegate);
+
+  $("#fload").change(loader);
 }
 
 $(document).ready(setup);
